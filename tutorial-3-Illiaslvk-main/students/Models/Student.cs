582aa0c37f696a33484b3cb3a85f9cb180038940
Illia <101345368+Illiaslvk@users.noreply.github.com>
2022-12-15 21:43:38 +0100

    Add files via upload

---
+++ tutorial-3-Illiaslvk-main/students/Models/Student.cs
@@ -0,0 +1,117 @@
+﻿using Newtonsoft.Json;
+
+namespace students
+{
+    public class Student
+    {
+        private string _firstName;
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        private string _lastName;
+
+        public string LastName
+        {
+            get{ return _lastName; }
+            set{ _lastName = value; }
+        }
+
+        private string _index;
+        public string Index
+        {
+            get { return _index; }
+            set { _index = value; }
+        }
+
+        private DateTime _birthDate;
+
+        public string BirthDate
+        {
+            get { return _birthDate.ToString("MM/dd/yyyy"); }
+            set { _birthDate = StudentService.GetDateFromString(value); }
+        }
+
+        private string _studies;
+        public string Studies
+        {
+            get { return _studies; }
+            set { _studies = value; }
+        }
+
+        private string _mode;
+        public string Mode
+        {
+            get { return _mode; }
+            set { _mode = value; }
+        }
+
+        private string _email;
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        private string _fatherName;
+        public string FatherName
+        {
+            get { return _fatherName; }
+            set { _fatherName = value; }
+        }
+
+        private string _motherName;
+        public string MotherName
+        {
+            get { return _motherName; } 
+            set { _motherName = value;  }
+        }
+
+        [JsonConstructor]
+        public Student(string firstName,
+            string lastName,
+            string index,
+            string birthDate,
+            string studies,
+            string mode,
+            string email,
+            string fatherName,
+            string motherName)
+        {
+            this._firstName = firstName;
+            this._lastName = lastName;
+            this._index = index;
+            this._birthDate = StudentService.GetDateFromString(birthDate);
+            this._studies = studies;
+            this._mode = mode;
+            this._email = email;
+            this._fatherName = fatherName;
+            this._motherName = motherName;
+        }
+
+        public string ToCsvString()
+        {
+            return this._firstName + "," + this._lastName + "," + 
+                this._index + "," + StudentService.ConvertDateToString(this._birthDate) + "," +
+                this._studies + "," + this._mode + "," + this._email + "," +
+                this._fatherName + "," + this._motherName;
+        }
+
+        public void CheckIfNullFields()
+        {
+            if (this._firstName == null ||
+            this._lastName == null ||
+            this._index == null ||
+            this._studies == null ||
+            this._mode == null ||
+            this._email == null ||
+            this._fatherName == null ||
+            this._motherName == null)
+            {
+                throw new ArgumentNullException();
+            }
+        }
+    }
+}
---
+++ tutorial-3-Illiaslvk-main/students/Services/StudentService.cs
@@ -0,0 +1,146 @@
+﻿using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+
+namespace students
+{
+    public class StudentService
+    {
+        private const string PATH_TO_DATABASE = "students.csv";
+        private readonly StudentJsonValidator validator = new();
+
+        public List<Student> GetAll()
+        {
+            var students = File.ReadAllLines(PATH_TO_DATABASE);
+            return students.Where(student => !student.Equals(""))
+                        .Select(student => ParseLineIntoStudent(student))
+                        .ToList<Student>();
+        }
+
+        public Student GetByIndex(string index)
+        {
+            using var reader = new StreamReader(PATH_TO_DATABASE);
+            Student? student = null;
+            string? line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (line.Contains(index))
+                {
+                    student = ParseLineIntoStudent(line);
+                    break;
+                }
+            }
+            reader.Close();
+            return student;
+        }
+
+        public Student Add(object postBody)
+        {
+            if (!validator.IsValid(postBody))
+            {
+                return null;
+            }
+            Student? student = JsonConvert.DeserializeObject<Student>(postBody.ToString());
+
+            using var reader = new StreamReader(PATH_TO_DATABASE);
+            string? line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (line.Contains(student.Index))
+                {
+                    reader.Close();
+                    return null;
+                }
+            }
+            reader.Close();
+            File.AppendAllLines(PATH_TO_DATABASE, new List<string>() { student.ToCsvString() });
+            return student;
+        }
+
+        public Student Modify(object studentObj)
+        {
+            if (!validator.IsValid(studentObj))
+            {
+                return null;
+            }
+            Student? student = JsonConvert.DeserializeObject<Student>(studentObj.ToString());
+
+            string? line = File.ReadAllLines(PATH_TO_DATABASE)
+                .Where(line => line.Contains(student.Index))
+                .First();
+
+            if (line == null)
+            {
+                return null;
+            }
+
+            string index = getIndexFromCsv(line);
+            Delete(index);
+            return Add(studentObj);
+        }
+
+        private static string getIndexFromCsv(string studentCsv)
+        {
+            Regex rx = new Regex(@"s[0-9]+");
+            MatchCollection matches = rx.Matches(studentCsv);
+            return matches.First().Value;
+        }
+
+        public static DateTime GetDateFromString(string date)
+        {
+            try
+            {
+                var dateParts = date.Split("/");
+                var month = dateParts[0].Length == 1 ? "0" + dateParts[0] : dateParts[0];
+                var day = dateParts[1].Length == 1 ? "0" + dateParts[1] : dateParts[1];
+                var year = dateParts[2];
+
+                var properFormat = month + "/" + day + "/" + year;
+
+                return DateTime.ParseExact(properFormat, "MM/dd/yyyy", null);
+            } catch (IndexOutOfRangeException e)
+            {
+                throw new ArgumentException(date, nameof(date), e);
+            }
+
+        }
+
+        public static string ConvertDateToString(DateTime date)
+        {
+            return date.Month + "/" + date.Day + "/" + date.Year;
+        }
+
+        public int Delete(string index)
+        {
+            try
+            {
+                var lines = File.ReadAllLines(PATH_TO_DATABASE).Where(line => !line.Contains(index));
+                File.WriteAllLines(PATH_TO_DATABASE, lines);
+                return 0;
+
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+        }
+
+        private static Student ParseLineIntoStudent(string line)
+        {
+            var args = line.Split(",");
+
+            return new Student
+            (
+                args[0],
+                args[1],
+                args[2],
+                args[3],
+                args[4],
+                args[5],
+                args[6],
+                args[7],
+                args[8]
+            );
+        }
+
+    }
+}
